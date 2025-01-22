8cf0351b0769fb59ee559d8e173bf3994a0ab87c
Dwij <42746391+Dwij9@users.noreply.github.com>
2025-01-22 16:39:32 -0500

    done

--- java/src/main/java/org/example/InsertionSort.java
+++ java/src/main/java/org/example/InsertionSort.java
@@ -1,16 +1,25 @@
 package org.example;
 
 public class InsertionSort {
-    //Sorts the array using Insertion Sort.
+    // Sorts the array using Insertion Sort.
     public void sort(Comparable[] a) {
         for (int i = 1; i < a.length; i++) {
-            insert(i, a);
+            insert(i, a); // Insert the current element into the sorted portion
         }
     }
 
     private void insert(int i, Comparable[] a) {
-        //Inserts the 'Transition element' into its correct position in the sorted portion of the array.
-        //TODO: TO BE IMPLEMENTED
+        Comparable key = a[i]; // The element to be inserted into the sorted part
+        int j = i - 1;
+
+       
+        while (j >= 0 && a[j].compareTo(key) > 0) {
+            a[j + 1] = a[j]; 
+            j--; 
+        }
+
+        
+        a[j + 1] = key;
     }
 
     private void swap(Object[] a, int i, int j) {
